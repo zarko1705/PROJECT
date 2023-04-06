9b0fc1ebfd35c86263d5e041805b6d1008fa0e82
Žarko Petrović <zarko.petrovic1705@gmail.com>
2023-04-06 16:19:47 +0200

    init

--- Gallery_Site/script.js
+++ Gallery_Site/script.js
@@ -1,11 +1,11 @@
-const galleryItem = document.querySelectorAll(".gallery-item");
+const galleryItems = document.querySelectorAll(".gallery-item");
 const modal = document.querySelector("#modal");
 const modalImage = document.querySelector("#modal-image");
 
-galleryItem.forEach((item) => {
+galleryItems.forEach((item) => {
     item.addEventListener("click", () => {
         modalImage.src = item.querySelector("img").src;
-        modal.style.displey = "block";
+        modal.style.display = "block";
     });
 });
 
